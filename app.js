d8e21a4e92179dfc26f4c87c8b093a02323749ed
Lucas Gonzalez <gonzalezlucasnicolas69@gmail.com>
2023-09-12 20:42:35 -0300

    detalle que me olvide de subir

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ server.listen(8080, () => {
 
 app.post('/api/productos/add', (req, res) => {
     const product = req.body;
-    
+
     if (!product.title || !product.description || !product.price || !product.thumbnail || !product.code || !product.stock) {
         return res.status(400).json({ mensaje: 'Todos los campos son obligatorios' });
     }
@@ -32,9 +32,6 @@ app.post('/api/productos/add', (req, res) => {
 
     return res.status(201).json({ mensaje: 'Producto agregado correctamente' });
 });
-    // productManager.addProduct();
-    // res.status(200).json({ success: `producto ${product} creado loro` });
-
 
 app.get('/', (req, res) => {
     res.send(' esta es la pag principal ');
@@ -48,16 +45,16 @@ app.get('/api/productos', (req, res) => {
 app.get('/api/productos/:id', (req, res) => {
 
     const productId = productManager.getProductById(parseInt(req.params.id));
-      if (!productId) {
+    if (!productId) {
         return res.status(404).json({ mensaje: 'Producto no encontrado' });
     }
     res.send(productId);
 });
 
-app.delete ('/api/productos/delete/:id', (req, res)=>{
+app.delete('/api/productos/delete/:id', (req, res) => {
     const productId = parseInt(req.params.id);
     const deletedProduct = productManager.getProductById(productId);
-    if (deletedProduct){
+    if (deletedProduct) {
         productManager.deleteProduct(productId);
         return res.status(200).json({ mensaje: 'Producto borrado' });
     } else {
@@ -65,31 +62,43 @@ app.delete ('/api/productos/delete/:id', (req, res)=>{
     }
 });
 
+app.put('/api/productos/put/:id', (req, res) => {
+    const productId = parseInt(req.params.id);
+    const updatedFields = req.body;
+    const existingProduct = productManager.getProductById(productId);
+
+    if (existingProduct) {
+        productManager.updateProduct(productId, updatedFields);
+        return res.status(200).json({ mensaje: 'Producto editado' });
+    } else {
+        return res.status(404).json({ mensaje: 'Producto no encontrado' });
+    }
+});
 
 // manejo de carts---------------------
 
 app.post('/api/cart', (req, res) => {
 
     cartManager.addCart();
-    res.status(200).json({ success: `carrito ${cartManager.nextId-1} creado` });
+    res.status(200).json({ success: `carrito ${cartManager.nextId - 1} creado` });
 });
 
-app.delete ('/api/cart/delete/:id', (req, res)=>{
+app.delete('/api/cart/delete/:id', (req, res) => {
     const cartId = parseInt(req.params.id);
-    const cartExists = cartManager.cartExists (cartId)
+    const cartExists = cartManager.cartExists(cartId)
 
     if (cartExists) {
         cartManager.deleteCart(cartId);
         return res.status(200).json({ mensaje: `Carrito ${cartId} fue borrado` });
     } else {
-        return res.status(404).json({ mensaje: `Carrito ${cartId} no encontrado`});
-        
+        return res.status(404).json({ mensaje: `Carrito ${cartId} no encontrado` });
+
     }
 });
 
 
-  // cartManager.deleteCart(parseInt(req.params.id));
-    // return res.status(404).json({ mensaje: 'carrito borrado' });
+// cartManager.deleteCart(parseInt(req.params.id));
+// return res.status(404).json({ mensaje: 'carrito borrado' });
 
 
 
